65d37cc14bd829e002901dd72ede31891b763ab1
tahara yo <tahara.you@ring-tech.jp>
2023-10-13 05:30:19 +0900

    Initial Commit

---
+++ StudyApp-Login/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  StudyApp-Login
+//
+//  Created by yotahara on 2023/10/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .black
+    }
+
+
+}
+
---
+++ StudyApp-Login/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  StudyApp-Login
+//
+//  Created by yotahara on 2023/10/13.
+//
+
+import Foundation
