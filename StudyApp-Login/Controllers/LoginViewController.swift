5f1b3f181cad9356f9c508b3efd10208e0ff6278
tahara yo <tahara.you@ring-tech.jp>
2023-10-13 09:18:49 +0900

    LoginViewControllerの実装見取図をコメントで提示

--- StudyApp-Login/Controllers/LoginViewController.swift
+++ StudyApp-Login/Controllers/LoginViewController.swift
@@ -9,10 +9,102 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    // FirebaseAuth
+    private let authModel: AuthModel!
+    // Validator
+    private let validatorModel: ValidatorModel!
+
+    // MARK: - UIs
+    
+    // titleLabel
+    var titleLabel: UILabel!
+    // emailTextField
+    var emailTextField: UITextField!
+    // passwordTextField
+    var passwordTextField: UITextField!
+    // loginButton
+    var loginButton: UIButton!
+    // switchViewButton
+    var switchViewButton: UIButton!
+    // resetPasswordButton
+    var resetPasswordButton: UIButton!
+    
+    // MARK: - Initializer
+
+    init(authModel: AuthModel, validatorModel: ValidatorModel) {
+        self.authModel = authModel
+        self.validatorModel = validatorModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        // storyboardからの生成は想定しない
+        fatalError("Not for Storyboard")
+    }
+    
+    // MARK: - Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        addSubviews()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    // setupUI: UIコンポーネントの初期化
+    func setupUI() {
+        // titleLabel
+        // emailTextField
+        // passwordTextField
+        // loginButton
+        // switchViewButton
+        // resetPasswordButton
+    }
+    
+    // addSubviews: 初期化したUIコンポーネントをviewに追加
+    func addSubviews() {
+        // titleLabel
+        // emailTextField
+        // passwordTextField
+        // loginButton
+        // switchViewButton
+        // resetPasswordButton
+    }
+    
+    // setupLayout: UIコンポーネントのレイアウトを設定
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            // titleLabel
+            // emailTextField
+            // passwordTextField
+            // loginButton
+            // switchViewButton
+            // resetPasswordButton
+        ])
+    }
+    
+    // MARK: - Actions
+    
+    // loginButtonのタップ処理
+    func tappedloginButton() {
         
     }
+    
+    // switchViewButtonのタップ処理
+    func tappedSwitchViewButton() {
+        
+    }
+    
+    // resetPasswordButtonのタップ処理
+    func tappedResetPasswordButton() {
+        
+    }
+    
+    // MARK: - Delegate: TextField
 }
