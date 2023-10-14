03f25f9194a845fe7b97764fc20aa764de3c4e73
tahara yo <tahara.you@ring-tech.jp>
2023-10-15 07:04:56 +0900

    FirebaseSDKの追加

--- StudyApp-Login/AppDelegate.swift
+++ StudyApp-Login/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore // 追加
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure() // 追加
+
         return true
     }
 
